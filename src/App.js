12f1b6985ffd3c379b8c4af21cd0a0a212a76960
= <Emmanuelsumeh@gmail.com>
2021-12-31 23:14:25 +0100

    feat: fixed smart contract issues and optimized frontend

--- src/App.js
+++ src/App.js
@@ -10,11 +10,9 @@ import verifier from "./contracts/verifier.abi.json";
 import IERC from "./contracts/ierc.abi.json";
 import Products from "./components/Products";
 import AddProduct from "./components/AddProduct";
+import {ERC20_DECIMALS, cUSDContractAddress, contractAddress} from "./utils/constants";
 
-const ERC20_DECIMALS = 18;
 
-const contractAddress = "0xA2a29121E518186088d51f8F36494de48ACCC9Fb";
-const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
 function App() {
   const [contract, setcontract] = useState(null);
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+
+const ERC20_DECIMALS = 18;
+
+// const contractAddress = "0xA2a29121E518186088d51f8F36494de48ACCC9Fb";
+const contractAddress = "0xa8AfE9eb4c96DfD2492ACf64761D8C90634B9943";
+
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
+
+export {ERC20_DECIMALS, contractAddress, cUSDContractAddress}
